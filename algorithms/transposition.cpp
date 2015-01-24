6378ae92a23755b12a742729dd2dd269d6430e53
aaronschraner <aaron@pnwcab.com>
2015-01-24 03:27:09 -0800

    started on transposition algorithm

--- algorithms/transposition.cpp
+++ algorithms/transposition.cpp
@@ -8,12 +8,23 @@ Description: Transposition (en/de)cryption algorithms
 //TODO: you don't need to padd it with z's
 #include "../algorithms.h"
 //TODO: doesn't currently encrypt or decrypt anything
+unsigned int textLength(const char text[])
+{
+	for(int i=0;i<MAX_MSG_SIZE; i++)
+	{
+		if(text[i]=='\0')
+			return i;
+	}
+	return 0;
+}
 void  transpositionEncrypt( const char  plaintext[], char ciphertext[], unsigned key )
 {
 	//fdsaf
+	int ptLen=textLength(plaintext);
 	for(int i=0;i<MAX_MSG_SIZE;i++)
 	{
-		ciphertext[i]=plaintext[i];
+		//ciphertext[i]=plaintext[i];
+		ciphertext[i]=(i<ptLen?plaintext[(i%key)*key+(i/key)]): //probably doesn't work
 	}
 }
 
