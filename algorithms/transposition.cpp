4f8ca36177bd17c6b87c515f29e89f19e062c429
aaronschraner <aaron@pnwcab.com>
2015-01-29 01:39:23 -0800

    Mostly fixed transposition algorithms

--- algorithms/transposition.cpp
+++ algorithms/transposition.cpp
@@ -22,71 +22,16 @@ unsigned int textLength(const char text[])
 }
 
 
-//
-//
-// 0 1 2 3 4 5 6 7 8 9
-//   x--->
-//
-// y 0 1 2
-// | 3 4 5
-// | 6 7 8
-// V 9 _ _
-//
-// 0 3 6 9 1 4 7 _ 2 5 8 _
-//
-// test case:
-// for plaintext length of 10 (0123456789)
-// and key=3
-//  x->
-//
-//y 0 1 2
-//| 3 4 5
-//V 6 7 8
-//  9 _ _
-//
-// --> 
-// y ->
-// x 0 3 6 9
-// | 1 4 7 _
-// V 2 5 8 _
-// 0 1 2 3 4 5 6 7 8 9 A B
-// 0 3 6 9 1 4 7 A 2 5 8 B
-//
-// inpos -> outpos
-// 0 -> 0
-// 1 -> 4
-// 2 -> 8
-// 3 -> 1
-// 4 -> 5
-// 5 -> 9
-// 6 -> 2
-// 7 -> 6
-// 8 -> 
-//
-// yMax=4
-// xMax=3
-//
-// outpos -> inpos
-// 0 -> 0
-// 1 -> 3
-// 2 -> 6
-// 3 -> 9
-// 4 -> 1
-// 5 -> 4
-// 6 -> 7
-// 7 -> A
-// 8 -> 2
-// 9 -> 5
-// X => 8
-// 1 => B
-//
-// (o%4)*3 + o/4
+
+//doesn't work in some cases for some reason
+
 void  transpositionEncrypt( const char  plaintext[], char ciphertext[], unsigned key )
 {
 	//fdsaf
 	const unsigned int ptLen = textLength(plaintext);
+	
 	const unsigned int xMax  = key;
-	const unsigned int yMax  = ptLen / key + ( ptLen%key > 0 ? 1 : 0);
+	const unsigned int yMax  = (ptLen+key-1) / key ;//+ (ptLen%key > 0 ? 1 : 2);
 
 #ifdef DEBUG
 	std::cout << "DEBUG: \nxMax = " << xMax << "\nyMax = " << yMax << "\nptLen = " << ptLen << "\n";
@@ -101,14 +46,20 @@ void  transpositionEncrypt( const char  plaintext[], char ciphertext[], unsigned
 			ciphertext[ y + yMax * x ] = ( x + xMax * y < ptLen ? plaintext[ x + xMax * y ] : '_' );
 		}
 	}
+
 }
 
 void  transpositionDecrypt( const char  ciphertext[], char plaintext[], unsigned key )
 {
-	unsigned int ptLen = textLength(plaintext);
-	unsigned int xMax  = key;
-	unsigned int yMax  = ptLen / key + ( ptLen%key > 0 ? 1 : 0);
+	//fdsaf
+	const unsigned int ptLen = textLength(ciphertext);
+	
+	const unsigned int xMax  = key;
+	const unsigned int yMax  = ptLen / key ;//+ (ptLen%key > 0 ? 1 : 2);
 
+#ifdef DEBUG
+	std::cout << "DEBUG: \nxMax = " << xMax << "\nyMax = " << yMax << "\nptLen = " << ptLen << "\n";
+#endif
 	for(unsigned y=0;y<yMax;y++)
 	{
 		for(unsigned x=0;x<xMax;x++)
@@ -119,5 +70,5 @@ void  transpositionDecrypt( const char  ciphertext[], char plaintext[], unsigned
 			plaintext[ x + xMax * y ] = ciphertext[ y + yMax * x ];
 		}
 	}
-}
 
+}
