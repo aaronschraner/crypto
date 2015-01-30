9a1de27039a437d44875e545f76f2455a9dc6e3a
aaronschraner <aaron@pnwcab.com>
2015-01-29 16:46:17 -0800

    added comments where needed

--- algorithms/caesar.cpp
+++ algorithms/caesar.cpp
@@ -8,34 +8,43 @@ Description: Caesar (en/de)cryption algorithms
 #include "../algorithms.h"
 void caesarEncrypt(const char plaintext[], char ciphertext[], int key)
 {
-	key=NUM_CHARACTERS-((-key)%NUM_CHARACTERS); //range checking
-	key=key%NUM_CHARACTERS;
+	key=NUM_CHARACTERS-((-key)%NUM_CHARACTERS); //Ensure key is greater than -26
+	key=key%NUM_CHARACTERS; //ensure key is less than 26
+
 	//encrypt using caesar
+	//iterate through message size
 	for(int i=0;i<MAX_MSG_SIZE;i++)
 	{
 		if (!CHAR_OUT_OF_RANGE(plaintext[i]))
 		{
-			ciphertext[i]=(plaintext[i]-MIN_ASCII_VALUE + key)%NUM_CHARACTERS+MIN_ASCII_VALUE; //add key to value and ensure output is within range
+			ciphertext[i]=(plaintext[i]-MIN_ASCII_VALUE + key)%NUM_CHARACTERS+MIN_ASCII_VALUE; 
+			//add key to value and ensure output is within range
 		}
-		else
+		else //if the plaintext character is out of range
 		{
-			ciphertext[i]=plaintext[i];
+			ciphertext[i]=plaintext[i]; //just use that character
 		}
 	}
 }
 
 void caesarDecrypt(const char ciphertext[], char plaintext[], int key)
 {
-	key=NUM_CHARACTERS-((-key)%NUM_CHARACTERS); //range checking
+
+	key=NUM_CHARACTERS-((-key)%NUM_CHARACTERS); 
 	key=key%NUM_CHARACTERS;
+	//ensure -26 < key < 26
+	
 	// decrypt using caesar
+	// iterate through message size
 	for(int i=0;i<MAX_MSG_SIZE;i++)
 	{
 		if (!CHAR_OUT_OF_RANGE(ciphertext[i])) 
 		{
-			plaintext[i]=(ciphertext[i]-MIN_ASCII_VALUE+NUM_CHARACTERS-key%NUM_CHARACTERS)%NUM_CHARACTERS+MIN_ASCII_VALUE; //subtract key from value and ensure output is within range
+			plaintext[i]=(ciphertext[i]-MIN_ASCII_VALUE+NUM_CHARACTERS-key%NUM_CHARACTERS)%NUM_CHARACTERS+MIN_ASCII_VALUE; 
+			//subtract key from value and ensure output is within range
 		}
-		else
+		else 
+		//again if plaintext is out of range use the character without encrypting it
 		{
 			plaintext[i]=ciphertext[i];
 		}
